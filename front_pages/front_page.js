ef1cecb7b567acbd5122013e5978c94effc6b8d3
Chuen Ley Low <chuenleylow@gmail.com>
2020-08-16 22:06:12 +1000

    Added fade in for title

--- front_pages/front_page.js
+++ front_pages/front_page.js
@@ -26,3 +26,33 @@ function player_input(){
     console.log(joined_list);
     
 }
+
+var opacity_title = 0;
+var opacity_sub = 0;
+var intervalID = 0;
+
+function interval(){
+    intervalID = setInterval(fade_in, 70);
+
+}
+
+function fade_in(){
+    var title = document.getElementById("title");
+    var subtitle = document.getElementById("btm-title");
+
+    opacity_title = Number(window.getComputedStyle(title).getPropertyValue("opacity"));
+    opacity_sub = Number(window.getComputedStyle(subtitle).getPropertyValue("opacity"));
+
+    if (opacity_title < 1){
+        opacity_title += 0.03;
+        opacity_sub += 0.03;
+        title.style.opacity = opacity_title;
+        subtitle.style.opacity = opacity_sub;
+
+    }
+    else{
+        clearInterval(intervalID);
+    }
+}
+
+interval();
