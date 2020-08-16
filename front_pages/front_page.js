a0ebf46a3c2f507ae6dcbadf30712c61a9b15bba
Chuen Ley Low <chuenleylow@gmail.com>
2020-08-16 23:26:06 +1000

    final animation for front

--- front_pages/front_page.js
+++ front_pages/front_page.js
@@ -36,15 +36,17 @@ var opacity_instruction = 0;
 var opacity_joined = 0;
 var opacity_play = 0;
 var opacity_input = 0;
+var opacity_name = 0;
 var intervalID = 0;
 
 function interval(){
-    intervalID = setInterval(fade_in, 70);
+    intervalID = setInterval(fade_in, 60);
 
 }
 
+
 function fade_in(){
-    var title = document.getElementById("title");
+    
     var subtitle = document.getElementById("btm-title");
     var game = document.getElementById("game");
     var definition = document.getElementById("definition");
@@ -52,9 +54,9 @@ function fade_in(){
     var instruction = document.getElementById("instruction");
     var joined = document.getElementById("joined");
     var play = document.getElementById("play");
-    var input = document.querySelector(".player-input")
+    var input = document.querySelector(".player-input");
+    var name = document.getElementById("name-prompt");
 
-    opacity_title = Number(window.getComputedStyle(title).getPropertyValue("opacity"));
     opacity_sub = Number(window.getComputedStyle(subtitle).getPropertyValue("opacity"));
     opacity_game = Number(window.getComputedStyle(game).getPropertyValue("opacity"));
     opacity_def = Number(window.getComputedStyle(definition).getPropertyValue("opacity"));
@@ -63,19 +65,20 @@ function fade_in(){
     opacity_joined = Number(window.getComputedStyle(joined).getPropertyValue("opacity"));
     opacity_play = Number(window.getComputedStyle(play).getPropertyValue("opacity"));
     opacity_input = Number(window.getComputedStyle(input).getPropertyValue("opacity"));
+    opacity_name = Number(window.getComputedStyle(name).getPropertyValue("opacity"));
 
-    if (opacity_title < 1){
-        opacity_title += 0.03;
-        opacity_sub += 0.03;
-        opacity_game += 0.03;
-        opacity_def += 0.03;
-        opacity_subdef += 0.03;
-        opacity_instruction += 0.03;
-        opacity_joined += 0.03;
-        opacity_play += 0.03;
-        opacity_input += 0.03;
+    if (opacity_sub < 1){
+        
+        opacity_name += 0.02;
+        opacity_sub += 0.02;
+        opacity_game += 0.02;
+        opacity_def += 0.02;
+        opacity_subdef += 0.02;
+        opacity_instruction += 0.02;
+        opacity_joined += 0.02;
+        opacity_play += 0.02;
+        opacity_input += 0.02;
 
-        title.style.opacity = opacity_title;
         subtitle.style.opacity = opacity_sub;
         game.style.opacity = opacity_game;
         definition.style.opacity = opacity_def;
@@ -84,6 +87,7 @@ function fade_in(){
         joined.style.opacity = opacity_joined;
         play.style.opacity = opacity_play;
         input.style.opacity = opacity_input;
+        name.style.opacity = opacity_name;
 
     }
     else{
