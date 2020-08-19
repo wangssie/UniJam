67ce83dfd0ca7fff17eb5f04e8446fd73436d9b4
Serena Wang <serenaw@student.unimelb.edu.au>
2020-08-19 21:35:19 +1000

    skeleton of startround method done

--- serena_trialling/main.js
+++ serena_trialling/main.js
@@ -1,9 +1,18 @@
-// define constants 
-let players = []
-const maxPlayers = 2;
-const Player = require('./player');
-const roundTotal = 5;
+// imports
 const Timer = require('easytimer.js').Timer;
+const WordDatabase = require('./WordDatabase');
+const Player = require('./player');
+const addScore = require('./player').addScore;
+// game constants  
+const maxPlayers = 2;
+const roundTotal = 5;
+const section1Timelimit = 16;
+// global variables 
+let players = [];
+let wordsUsed = [];
+let currentPlayersList = [];
+// html constants 
+
 
 // the primary engine of the game 
 function main() {
@@ -12,16 +21,18 @@ function main() {
     // while ***play button not clicked || !enoughPlayers()
         // ***event listener for player creation (aka when input is made)
             // ***event listener triggered function 
-            createPlayer();
+            createPlayer('username1');
+            createPlayer('username2');
 
     
     //part 2: the game 
     for (let i=0; i<=roundTotal; i++) {
-        for (let j=0; j<=players.length; j++) {
-            startRound();
+        for (let j=0; j<players.length; j++) {
+            startRound(players[j]);
             checkRound();
             // while ***play button not clicked 
                 // do nothing 
+            cleanRound();
         }
     }
 
@@ -53,16 +64,39 @@ function enoughPlayers() {
 }
 
 // displays playing page 
-function startRound() {
-    // create timer 
-    section1(timer);
-    // event listener for when timer is done 
-    section2();
+function startRound(player) {
+    var timer = new Timer();
+    timer.start({startValues: [0, section1Timelimit, 0, 0, 0], target: [0,0,0,0,0], countdown : true})
+    section1(player);
+    timer.addEventListener('secondsUpdated', displaySeconds())
+    // section2 begins when timer is done
+    timer.addEventListener('stopped', section2(player));
+}
+
+function section1(player) { 
+    // CHANGE
+    // when input is made, add to array
+    var word;
+    currentPlayersList.push(word);
+    player.addScore();
+
+}
+
+// changes the seconds on the html 
+function displaySeconds() {}
+
+function section2(player) {
+
 }
 
 // page where other players checks if words are valid 
 function checkRound() {
 
+}
+
+// cleans the variables to get ready for next players round
+function cleanRound() {
+    
 }
 
 // displays the ending page with players scores and option to play again
