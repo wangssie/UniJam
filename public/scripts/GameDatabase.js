42dfd98b8160b96564efa0f543c29c6efafe3900
Serena Wang <serenaw@student.unimelb.edu.au>
2020-08-21 00:21:58 +1000

    added global constants to gamedatabase class

--- public/scripts/GameDatabase.js
+++ public/scripts/GameDatabase.js
@@ -5,4 +5,8 @@ module.exports = class GameDatabase {
     static roundsPlayed = 0;
     static currentPlayer = 0; 
     static lastWord = '';
+
+    static maxPlayers = 2;
+    static roundTotal = 5;
+    static section1Timelimit = 15; 
 }
