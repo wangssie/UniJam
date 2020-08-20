ff8831c27fe8b9cb24946e2de8c414fea19956d8
Serena Wang <serenaw@student.unimelb.edu.au>
2020-08-20 14:06:13 +1000

    Fixed bugs

--- serena_trialling/check.js
+++ serena_trialling/check.js
@@ -1,6 +1,10 @@
+const GameDatabase = require('./GameDatabase');
+const Player = require('./player');
+
+
 // page where other players checks if words are valid 
 function checkRound() {
-    currentPlayersList.push(word); // add to player's current list of words
+    GameDatabase.currentPlayersList.push(GameDatabase.lastWord); // add to player's current list of words
     // event listener *** for adding invalid words, evoke invalidWord()
     // event listener *** when done is clicked: nextRound() evoked
 }
@@ -8,13 +12,25 @@ function checkRound() {
 // evoked when player determines word in chain is invalid
 function invalidWord() {
     // *** crosses off word 
-    players[currentPlayer].removeScore(); // *** update changes score 
+    GameDatabase.players[GameDatabase.currentPlayer].removeScore(); // *** update changes score 
 }
 
 // evoked once checking stage is complete
 function nextRound() {
-    currentPlayersList = []; // clear list of current player's words
-    roundsPlayed+=currentPlayer; // will +1 once second player was last to play
-    currentPlayer = (currentPlayer+1)%players.length; // change player 
-    startRound(); 
+    
+    GameDatabase.currentPlayersList = []; // clear list of current player's words
+    GameDatabase.roundsPlayed+=GameDatabase.currentPlayer; // will +1 once second player was last to play
+    GameDatabase.currentPlayer = (GameDatabase.currentPlayer+1)%GameDatabase.players.length; // change player 
+    GameDatabase.lastWord = '';
+    // *** EVOKE NEXT PLAY HTML
 }
+
+
+checkRound();
+
+//debugging
+/*
+GameDatabase.players = [new Player('swag')]
+nextRound();
+invalidWord();
+*/
