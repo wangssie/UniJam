f2001197510f3a44bf8eb7b633648a4e7202940b
Serena Wang <serenaw@student.unimelb.edu.au>
2020-08-21 20:47:08 +1000

    remove scripts in html to .js

--- public/scripts/play_page.js
+++ public/scripts/play_page.js
@@ -1,6 +1,6 @@
 var word_path = [];
-
-var start_word = "somerandomword";
+var allowSubmit = false;
+var timerLimit1 =16;
 
 function show_start_word(){
 
@@ -23,14 +23,57 @@ input.addEventListener("keyup", function(event) {
 
 function add_to_path(){
 
-    let input_word = input.value;
-    console.log(input_word);
-    
-    word_path.push(input_word);
+    if (allowSubmit) {
+      let input_word = input.value;
+      console.log(input_word);
+      
+      word_path.push(input_word);
 
-    document.getElementById("path-container").innerHTML += "<h3 id = 'path-format'>" + input_word + "</h3>";
-    console.log(word_path);
+      document.getElementById("path-container").innerHTML += "<h3 id = 'path-format'>" + input_word + "</h3>";
+      console.log(word_path);
 
-    input.value = "";
+      input.value = "";
+    }
 
 }
+
+// not allow any submissions to the word chain
+function closeSubmit() {
+  allowSubmit = false;
+}
+
+// allow submissions to the word chain
+function openSubmit() {
+  allowSubmit = true;
+}
+
+
+// 5 section countdown for the round to begin 
+var timer = setInterval(timerDecrease, 5000);
+
+// starting word is shown, submission is available 
+
+
+var timer = setInterval(timerDecrease, 1000);
+    
+function timerDecrease() {
+      timerLimit1--;
+      document.getElementById('timer').innerHTML = ("0"+timerLimit1).slice(-2);
+  }
+  var timer_stop = setTimeout(cancelTimer, timerLimit1*1000);
+  function cancelTimer() {
+      clearInterval(timer);
+      // trigger function that stops submission option
+      setTimeout(section2, 5000)
+      document.getElementById('timer').innerHTML = "GET READY FOR PART 2"
+      closeSubmit();
+  }
+  
+  function section2() {
+      timer = setInterval(timerIncrease, 1000) 
+      openSubmit();
+  }
+  function timerIncrease() {
+      timerLimit1++;
+      document.getElementById('timer').innerHTML = ("0"+timerLimit1).slice(-2);
+  }
